f16830cf335b4231fda0f604c3874ca6d9dbefac
Ammar Al-Najjar <91396720+Ammar-1993@users.noreply.github.com>
2025-12-02 14:59:31 +0300

    Refactor JobType enum to simplify job types
    Removed internship and freelance job types, and updated case names to match camel case convention.

--- src/Enums/JobType.php
+++ src/Enums/JobType.php
@@ -4,24 +4,8 @@ namespace App\Enums;
 
 enum JobType: string
 {
-    case FULL_TIME = 'Full-Time';
-    case PART_TIME = 'Part-Time';
-    case REMOTE = 'Remote';
-    case HYBRID = 'Hybrid';
-    case CONTRACT = 'Contract';
-    case INTERNSHIP = 'Internship';
-    case FREELANCE = 'Freelance';
-
-    public function label(): string
-    {
-        return match($this) {
-            self::FULL_TIME => 'Full-Time',
-            self::PART_TIME => 'Part-Time',
-            self::REMOTE => 'Remote',
-            self::HYBRID => 'Hybrid',
-            self::CONTRACT => 'Contract',
-            self::INTERNSHIP => 'Internship',
-            self::FREELANCE => 'Freelance',
-        };
-    }
+    case FullTime = 'Full-Time';
+    case Contract = 'Contract';
+    case Remote = 'Remote';
+    case Hybrid = 'Hybrid';
 }
